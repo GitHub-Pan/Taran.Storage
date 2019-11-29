760e16a3eb426c5f454b053ff24819f041349a2e
pan <panzhuyong@gmail.com>
2019-11-29 16:48:46 +0800

    add project

---
+++ src/Taran.Storage.Cmd/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Taran.Storage.Cmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
